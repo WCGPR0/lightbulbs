4b6c7d4560468a698b352e30fab818675e2d533b
WCGPR0 <victor_moove@hotmail.com>
2019-12-02 22:43:36 -0800

    Updated back to CliServer

--- Startup.cs
+++ Startup.cs
@@ -66,8 +66,8 @@ namespace lightbulbs
 
                 if (env.IsDevelopment())
                 {
-                    //spa.UseAngularCliServer(npmScript: "start");
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
+                    spa.UseAngularCliServer(npmScript: "start");
+                    //spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
                 }
             });
         }
